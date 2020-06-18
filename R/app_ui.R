e9b043351e62366d365695db8c321adc24b8e637
Konrad Zdeb <konrad.zdeb@me.com>
2020-06-19 00:00:10 +0100

    Basic dashboard

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
@@ -8,35 +8,37 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
-    fluidPage(
-      h1("DashboardBootstrap")
+    # List the first level UI elements here
+    dashboardPage(
+      dashboardHeader(title = "Basic dashboard"),
+      dashboardSidebar(),
+      dashboardBody(# Boxes need to be put in a row (or column)
+        fluidRow(
+          box(plotOutput("plot1", height = 250)),
+
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
+        ))
     )
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
-golem_add_external_resources <- function(){
-  
-  add_resource_path(
-    'www', app_sys('app/www')
-  )
- 
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'DashboardBootstrap'
-    )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
-  )
-}
+golem_add_external_resources <- function() {
+  add_resource_path('www', app_sys('app/www'))
 
+  tags$head(favicon(),
+            bundle_resources(path = app_sys('app/www'),
+                             app_title = 'DashboardBootstrap'))
+            # Add here other external resources
+            # for example, you can add shinyalert::useShinyalert() )
+}
